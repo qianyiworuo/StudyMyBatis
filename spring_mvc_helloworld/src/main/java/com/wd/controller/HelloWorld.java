0b3d9d613f5fe0dda4ba639a8bde38d234818aac
19307 <wangdiwrg@mail.com>
2022-08-23 20:43:29 +0800

    SpringMVC配置

--- spring_mvc_helloworld/src/main/java/com/wd/controller/HelloWorld.java
+++ spring_mvc_helloworld/src/main/java/com/wd/controller/HelloWorld.java
@@ -1,8 +1,22 @@
 package com.wd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorld {
+    @RequestMapping("/")
+    public String protal(){
+        //讲逻辑视图返回
+        return"index";
+    }
+    @RequestMapping("/test")
+    public String test(){
+        return "success";
+    }
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "success";
+//    }
 
 }
